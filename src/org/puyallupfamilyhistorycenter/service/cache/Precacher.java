3b595e85137de652a8f58419426537d6a7a93650
Graham Tibbitts <grahamtt@users.noreply.github.com>
2014-10-26 05:42:46 -0700

    Added second thread to precacher

--- src/org/puyallupfamilyhistorycenter/service/cache/Precacher.java
+++ src/org/puyallupfamilyhistorycenter/service/cache/Precacher.java
@@ -25,10 +25,11 @@
  */
 package org.puyallupfamilyhistorycenter.service.cache;
 
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -66,74 +67,84 @@ public class Precacher {
 
     private final String accessToken;
     private final int maxDepth;
-    private Future future;
+    private Set<Future> futures;
 
     public Precacher(String accessToken, int maxDepth) {
         this.accessToken = accessToken;
         this.maxDepth = maxDepth;
+        this.futures = new HashSet<>();
     }
 
     public void precache() {
-        future = executor.submit(new Runnable() {
-            FamilySearchFamilyTree tree = FamilyHistoryFamilyTree.getInstance(accessToken);
-            Queue<PrecacheObject> frontier = new LinkedList<>();
-            Queue<PrecacheObject> leafNodes = new LinkedList<>();
-            Set<String> currentLeafs = new HashSet<>();
+        final FamilySearchFamilyTree tree = FamilyHistoryFamilyTree.getInstance(accessToken);
+        final Queue<PrecacheObject> frontier = new ConcurrentLinkedQueue<>();
+        final Queue<PrecacheObject> leafNodes = new ConcurrentLinkedQueue<>();
+        final Set<String> currentLeafs = Collections.synchronizedSet(new HashSet<String>());
+        
+        PersonState person = tree.readPersonForCurrentUser();
+        frontier.add(new PrecacheObject(person.getPerson().getId(), 0));
 
-            {
-                PersonState person = tree.readPersonForCurrentUser();
-                frontier.add(new PrecacheObject(person.getPerson().getId(), 0));
-                
-                PersonSpousesState spouses = person.readSpouses();
-                if (spouses != null) {
-                    for (org.gedcomx.conclusion.Person spouse : spouses.getPersons()) {
-                        frontier.add(new PrecacheObject(spouse.getId(), 0));
-                    }
-                }
+        PersonSpousesState spouses = person.readSpouses();
+        if (spouses != null) {
+            for (org.gedcomx.conclusion.Person spouse : spouses.getPersons()) {
+                frontier.add(new PrecacheObject(spouse.getId(), 0));
             }
+        }
+        
+        for (int i = 0; i < 2; i++) {
+            futures.add(executor.submit(new Runnable() {
 
-            @Override
-            public void run() {
-                while (!frontier.isEmpty()) {
-                    try {
-                        PrecacheObject precacheObject = frontier.remove();
-                        Person person = source.get(precacheObject.id, accessToken);
-                        if (person.parents != null && precacheObject.depth < maxDepth) {
-                            for (PersonReference parent : person.parents) {
-                                System.out.println("Adding parent " + parent.getName() + " to frontier level " + (precacheObject.depth + 1));
-                                frontier.add(new PrecacheObject(parent.getId(), precacheObject.depth + 1));
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        while (!frontier.isEmpty()) {
+                            try {
+                                PrecacheObject precacheObject = frontier.remove();
+                                Person person = source.get(precacheObject.id, accessToken);
+                                if (person.parents != null && precacheObject.depth < maxDepth) {
+                                    for (PersonReference parent : person.parents) {
+                                        System.out.println(Thread.currentThread().getName() + ": Adding parent " + parent.getName() + " to frontier level " + (precacheObject.depth + 1));
+                                        frontier.add(new PrecacheObject(parent.getId(), precacheObject.depth + 1));
+                                    }
+                                } else {
+                                    System.out.println(Thread.currentThread().getName() + ": Adding self " + person.name + " to leaf nodes");
+                                    leafNodes.add(precacheObject);
+                                }
+                            } catch (Exception ex) {
+                                ex.printStackTrace(System.err);
                             }
-                        } else {
-                            System.out.println("Adding self " + person.name + " to leaf nodes");
-                            leafNodes.add(precacheObject);
                         }
-                    } catch (Exception ex) {
-                        ex.printStackTrace(System.err);
+                        try {
+                            Thread.sleep(6000);
+                        } catch (InterruptedException e) {
+                        }
                     }
-                }
 
-//                    while(!leafNodes.isEmpty()) {
-//                        PrecacheObject precacheObject = leafNodes.remove();
-//                        currentLeafs.remove(precacheObject.id);
-//                        Person person = source.get(precacheObject.id, accessToken);
-//                        if (person.children != null) {
-//                            for (PersonReference child : person.children) {
-//                                if (!currentLeafs.contains(child.getId())) {
-//                                    System.out.println("Adding child " + child.getName() + " to frontier");
-//                                    leafNodes.add(new PrecacheObject(child.getId(), precacheObject.depth - 1));
-//                                    currentLeafs.add(child.getId());
-//                                }
-//                            }
-//                        } else {
-//                            System.out.println("Person " + person.name + " has no children on record");
-//                        }
-//                    }
-            }
-        });
+    //                    while(!leafNodes.isEmpty()) {
+    //                        PrecacheObject precacheObject = leafNodes.remove();
+    //                        currentLeafs.remove(precacheObject.id);
+    //                        Person person = source.get(precacheObject.id, accessToken);
+    //                        if (person.children != null) {
+    //                            for (PersonReference child : person.children) {
+    //                                if (!currentLeafs.contains(child.getId())) {
+    //                                    System.out.println("Adding child " + child.getName() + " to frontier");
+    //                                    leafNodes.add(new PrecacheObject(child.getId(), precacheObject.depth - 1));
+    //                                    currentLeafs.add(child.getId());
+    //                                }
+    //                            }
+    //                        } else {
+    //                            System.out.println("Person " + person.name + " has no children on record");
+    //                        }
+    //                    }
+                }
+            }));
+        }
     }
 
     public void cancel() {
-        future.cancel(true);
+        for (Future future : futures) {
+            future.cancel(true);
+        }
         System.out.println("Cancelling precaching");
     }
 }
