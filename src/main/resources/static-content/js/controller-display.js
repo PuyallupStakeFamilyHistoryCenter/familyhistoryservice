775aed4fa9b0f0b051eca3be10bea79c86377644
Graham Tibbitts <grahamtt@users.noreply.github.com>
2017-09-03 11:34:04 -0600

    Fixed 'custom' button in puzzle

--- src/main/resources/static-content/js/controller-display.js
+++ src/main/resources/static-content/js/controller-display.js
@@ -448,14 +448,23 @@ function sendToServer(message) {
 
 function setHeaderName(headerName) {}
 
-    
+
 function initCheckboxes(selector, values) {
     $(selector).removeProp('checked').parent().removeClass("active");
     if (values) {
-        $.each(values, function(index, value) {
+        $.each(values, function (index, value) {
             console.info("Checking checkbox " + value);
-            $(selector + "[value="+value+"]").prop('checked',true).parent().addClass("active");
+            $(selector + "[value=" + value + "]").prop('checked', true).parent().addClass("active");
         });
+    }
+}
+
+
+function initCheckbox(selector, value) {
+    if (value) {
+        $(selector).prop('checked', value).parent().addClass("active");
+    } else {
+        $(selector).removeProp('checked').parent().removeClass("active");
     }
 }
 
